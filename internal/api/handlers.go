e95d83d1429a2a61c6aacb1c3927562f41052130
blazp04 <blaz.peric4@gmail.com>
2025-04-02 14:24:18 +0200

    update: cors settings

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -255,6 +255,7 @@ func SetupRouter(server *Server, jwtSecret string) *gin.Engine {
 			"http://localhost:3000",
 			"https://radionica.blazperic.com",
 			"https://radionica-switch-front.vercel.app",
+			"https://test-radionica.vercel.app",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "ngrok-skip-browser-warning"},
